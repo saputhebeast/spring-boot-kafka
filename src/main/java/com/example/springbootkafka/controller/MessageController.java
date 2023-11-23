da3a1135acca873c2d38bb76f8ac2bfde9cbfd8b
saputhebeast <sapumalwijekoon4@gmail.com>
2023-11-23 23:11:41 +0530

    Feat: Endpoint for sending messages

---
+++ src/main/java/com/example/springbootkafka/controller/MessageController.java
@@ -0,0 +1,26 @@
+package com.example.springbootkafka.controller;
+
+import com.example.springbootkafka.kafka.KafkaProducer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class MessageController {
+
+    private final KafkaProducer kafkaProducer;
+
+    public MessageController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @GetMapping("/publish")
+    public ResponseEntity<String> publish(@RequestParam("message") String message) {
+        kafkaProducer.sendMessage(message);
+        return new ResponseEntity<>("Message sent to the topic", HttpStatus.OK);
+    }
+}
